dbcf10e24c8903e7c61f0219570b202908b3896f
sanj231123 <sanj231123@gmail.com>
2026-03-02 18:26:51 +0530

    Added gitignore and removed terraform cache

---
+++ eks.tf
@@ -0,0 +1,32 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.31"
+
+  cluster_name    = local.name
+  cluster_version = "1.29"
+
+  cluster_endpoint_public_access = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  control_plane_subnet_ids = module.vpc.intra_subnets
+
+  enable_irsa = true
+
+  cluster_addons = {
+    vpc-cni    = { most_recent = true }
+    kube-proxy = { most_recent = true }
+    coredns    = { most_recent = true }
+  }
+
+  eks_managed_node_groups = {
+    dev_nodes = {
+      instance_types = ["t3.medium"]
+      min_size       = 2
+      max_size       = 3
+      desired_size   = 2
+      capacity_type  = "SPOT"
+    }
+  }
+}
---
+++ vpc.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+
+  name = "${local.name}-vpc"
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
+  intra_subnets   = local.intra_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
