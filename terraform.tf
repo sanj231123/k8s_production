6b7d28b88b688f0d3290f57caa760301e9cb6e06
sanjay <sanj231123@gmail.com>
2026-03-06 19:20:59 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -7,4 +7,12 @@ terraform {
       version = ">= 5.95.0, < 6.0.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "mahakal-bucket-sanjay-2026"   # your S3 bucket name
+    key            = "eks/dev/terraform.tfstate"    # state file path
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"         # your DynamoDB table
+    encrypt        = true
+  }
 }
